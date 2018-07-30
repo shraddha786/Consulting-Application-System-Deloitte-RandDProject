4c898bf88bae4da73b18dfd2a76b819f5c9537f6
Boi <you@example.com>
2018-07-30 15:46:03 +1200

    Added in mailgun API functionality and easy to use functionality, can bind email messaging to buttons and add addtional call information, cleaned code smell

--- start/public/js/chirpApp.js
+++ start/public/js/chirpApp.js
@@ -1,16 +1,17 @@
 //chirpApp.js
-var app = angular.module('chirpApp', ['ngRoute', 'ngResource', 'ngCookies']).run(function($rootScope, $http, $location, $cookies){
+
+var app = angular.module('chirpApp', ['ngRoute', 'ngResource', 'ngCookies']).run(function ($rootScope, $http, $location, $cookies) {
   $rootScope.authenticated = false;
   $rootScope.current_user = "";
 
-  $rootScope.logout = function(){
+  $rootScope.logout = function () {
     $http.get('/auth/signout');
     $rootScope.authenticated = false;
     $rootScope.current_user = "";
   };
 });
 
-app.config(function($routeProvider){
+app.config(function ($routeProvider) {
   $routeProvider
     //the timeline display
     .when('/', {
@@ -54,22 +55,22 @@ app.config(function($routeProvider){
     })
     //the MailGun display
     .when('/mailGun', {
-      templateUrl: 'mailGunner.html',
+      templateUrl: 'mailGunner.ejs',
       controller: 'mailController'
     });
 });
 
-app.factory('postService', function($resource){
+app.factory('postService', function ($resource) {
   return $resource('/api/posts/:id');
 });
 
-app.factory('uploadService', function($resource){
+app.factory('uploadService', function ($resource) {
 
   return $resource('/upload/:id');
 });
 
 //Cookie functionality showcase
-app.controller('cookieBaker', function($rootScope, $scope, $cookies){
+app.controller('cookieBaker', function ($rootScope, $scope, $cookies) {
 
   $cookies.put('userCookie', 'set');
   $cookies.put('authenCookie', 'set');
@@ -78,36 +79,41 @@ app.controller('cookieBaker', function($rootScope, $scope, $cookies){
   $scope.myUserCookie = $cookies.get('userCookie');
   $scope.myAuthCookie = $cookies.get('authCookie');
 
-  $scope.setCookie = function(val) {
-      $cookies.put('cookie', val);
+  $scope.setCookie = function (val) {
+    $cookies.put('cookie', val);
   }
 });
 
-app.controller('mainController', function($rootScope, $scope, postService){
+app.controller('mainController', function ($rootScope, $scope, postService) {
   $scope.posts = postService.query();
-  $scope.newPost = {created_by: '', text: '', created_at: ''};
+  $scope.newPost = { created_by: '', text: '', created_at: '' };
 
-  $scope.post = function(){
+  $scope.post = function () {
     $scope.newPost.created_by = $rootScope.current_user;
     $scope.newPost.created_at = Date.now();
-    postService.save($scope.newPost, function(){
+    postService.save($scope.newPost, function () {
       $scope.posts = postService.query();
-      $scope.newPost = {created_by: '', text: '', created_at: ''};
+      $scope.newPost = { created_by: '', text: '', created_at: '' };
     });
   };
 });
 
-app.controller('mailController', function($rootScope, $scope, postService){
-  
+app.controller('mailController', function ($rootScope, $scope, $http) {
+
+  $scope.mail = function () {
+    $http.get('/mailgun');
+    console.log("Success");
+  }
+
 });
 
-app.controller('authController', function($scope, $rootScope, $http, $location){
-  $scope.user = {username: '', password: ''};
+app.controller('authController', function ($scope, $rootScope, $http, $location) {
+  $scope.user = { username: '', password: '' };
   $scope.error_message = '';
 
-  $scope.login = function(){
-    $http.post('/auth/login', $scope.user).success(function(data){
-      if(data.user.username != ""){
+  $scope.login = function () {
+    $http.post('/auth/login', $scope.user).success(function (data) {
+      if (data.user.username != "") {
         $rootScope.authenticated = true;
         $rootScope.current_user = data.user.username;
 
@@ -116,24 +122,24 @@ app.controller('authController', function($scope, $rootScope, $http, $location){
     });
   };
 
-  $scope.register = function(){
-    $http.post('/auth/signup', $scope.user).success(function(data){
-        $rootScope.authenticated = true;
-        $rootScope.current_user = data.user.username;
+  $scope.register = function () {
+    $http.post('/auth/signup', $scope.user).success(function (data) {
+      $rootScope.authenticated = true;
+      $rootScope.current_user = data.user.username;
 
-        $location.path('/');
+      $location.path('/');
     });
   };
 });
 
-app.controller('fileController', function($rootScope, $scope, uploadService){
-   
+app.controller('fileController', function ($rootScope, $scope, uploadService) {
+
   $scope.files = uploadService.query();
-  
-  $scope.delete = function(){
+
+  $scope.delete = function () {
     var id = prompt("Please confirm the ID of the file:", "");
     var xhttp = new XMLHttpRequest();
-    xhttp.open("DELETE", "http://localhost:3000/upload/files/"+id, true);
+    xhttp.open("DELETE", "http://localhost:3000/upload/files/" + id, true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
 
